3b46d59136d310490f3535c336c1684b5a8a6bf1
Dipesh Joshi <joshidipesh12@gmail.com>
2021-10-20 00:18:00 +0530

    hill cipher documentation

--- modules/hill_cipher.py
+++ modules/hill_cipher.py
@@ -1,56 +1,22 @@
 from string import ascii_uppercase as English_Alphabet
-import numpy as np
+from utilities import matrix_inverse_Z26, string_to_Matrix_Z26
 import math
 
-from affine_cipher import inverse_Z26
 
+def encrypt(message_text, key):
+    """
+    Method Defined for ENCRYPTION of a Simple String message 
+    into a Cipher Text Using 2x2 Hill Cipher Technique
 
-def matrix_inverse_Z26(matrix: np.matrix):  # only for 2x2 matrix
-    if matrix.shape[0] != matrix.shape[1]:
-        print("Not Invertible Matrix Recieved!")
-        return
+    \nPARAMETERS\n
+    message_text: string to be encrypted
+    key: integer key for encryption
 
-    else:
-        determinant = None
-        try:
-            determinant = inverse_Z26(int(np.linalg.determinant(matrix)))
-        except:
-            print("Non-Invertible Matrix Recieved!")
-            return
+    \nRETURNS\n
+    cipher_text: encrypted Message string
+    """
 
-        matrix[0, 0], matrix[1, 1] = matrix[1, 1], matrix[0, 0]
-        for i in range(matrix.shape[1]):
-            for j in range(matrix.shape[0]):
-                matrix[i, j] = determinant*pow(-1, i+j)*matrix[i, j]
-
-        return matrix % 26
-
-
-def string_to_Matrix_Z26(message_text, m_rows, n_cols):
-    overflow_count = m_rows*n_cols - len(message_text)
-
-    if overflow_count < 0:
-        print("Invalid Input: m_rows x n")
-        return
-
-    else:
-        matrix = []
-        for i in message_text.upper():
-            if i in English_Alphabet:
-                matrix.append(English_Alphabet.index(i))
-            else:
-                print("Invalid Input\n")
-                return
-
-        matrix = [English_Alphabet.index(i) for i in message_text.upper()]
-        for _ in range(overflow_count):  # adding trailing Z as Dummy Alphabet
-            matrix.append(English_Alphabet.index("Z"))
-        matrix = np.matrix(matrix)
-
-        return matrix.reshape(m_rows, n_cols, order="F")
-
-
-def encrypt(message_text, key):  # for 2x2 Hill Cipher the length of key must be <= 4
+    # for 2x2 Hill Cipher the length of key must be <= 4
     # print("Warning: All Spaces with be lost!")
     cipher_text = ""
 
@@ -73,6 +39,18 @@ def encrypt(message_text, key):  # for 2x2 Hill Cipher the length of key must be
 
 
 def decrypt(cipher_text, key):
+    """
+    Method Defined for DECRYPTION of a String Cipher Text 
+    into the original message Using 2x2 Hill Cipher Technique
+
+    \nPARAMETERS\n
+    cipher_text: string to be decrypted
+    key: integer key used while encryption
+
+    \nRETURNS\n
+    message: decrypted Message string
+    """
+
     message_text = ""
 
     key_matrix = None
