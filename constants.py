a65e891bfd8b4e2d1c20333f52f37c32b32a8cc7
Dipesh Joshi <joshidipesh12@gmail.com>
2021-10-20 23:07:42 +0530

    fix: letter mismatch

--- constants.py
+++ constants.py
@@ -1,5 +1,11 @@
-"""module for centralized constants used 
+""" ## Constants
+    module for centralized constants used 
     all over the project.
 """
-ENGLISH_LETTERS_SORTED = "ETAOINSHRDLCUMWFGYPBVKJXQZ"
+
+# capital english letters
 ENGLISH_ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# English letters sorted according to
+# their frequencies in texts
+ENGLISH_LETTERS_SORTED = "ETAOINSHRDLCUMWFGYPBVKJXQZ"
